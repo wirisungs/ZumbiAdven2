8959e4d36fcfcd724ee1be4f2fbb2ef3960b1866
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2024-03-07 23:04:01 +0700

    Update Player Attack

--- Assets/Scripts/AttackManager.cs
+++ Assets/Scripts/AttackManager.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class AttackManager : MonoBehaviour
 {
-    [SerializeField] private int atkDamage = 2;
 
-    private bool isAttacking = false;
-    private float cooldownAttack = 0.5f;
+    private bool isAttacking = false;    
+
+   
     public Animator animator;
+
+    [Header("SFX")]
+    [SerializeField] private AudioClip attackSound;
+
+    [Header("Attack")]
+    [SerializeField] private int atkDamage = 2;
+    public float attackRange = 0.5f;
+    public Transform AttackPoint;
+    public LayerMask enemyLayers;
+    [SerializeField]private float cooldownAttack = 2f;
 
     public void Attack()
     {
         if (!isAttacking)
         {
+            SFXManager.instance.PlaySound(attackSound);
             animator.SetTrigger("AttackTrigger");
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, attackRange, enemyLayers);
+            foreach(Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<MonsterLife>().TakeDamage(atkDamage);
+            }
+
             isAttacking = true;
             StartCoroutine(ResetAttackAbility());
+
+            //Debug.Log("Hit");
         }
 
         Invoke("ResetAttackAbility", cooldownAttack);
@@ -25,5 +45,14 @@ public class AttackManager : MonoBehaviour
     {
         yield return new WaitForSeconds(cooldownAttack);
         isAttacking = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        if (AttackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(AttackPoint.position, attackRange);
     }
 }
